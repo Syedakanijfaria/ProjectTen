24c9504a8563dd342585144d5c518ed009e67ebe
SKFaria <fariafaria737@gmail.com>
2021-10-18 10:06:30 +0600

    problem facing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize.js';
+
+
+
+initializeAuthentication();
 
 function App() {
   return (
     <div className="App">
       <h2>hello how are</h2>
+      <p>firebase hosting</p>
+      <p>faltuuuuuuuuuuu</p>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAk1OifLU6CRwDR0K8K4M1M9BVPhvRKRok",
+    authDomain: "healthcare-related-websi-3ab7f.firebaseapp.com",
+    projectId: "healthcare-related-websi-3ab7f",
+    storageBucket: "healthcare-related-websi-3ab7f.appspot.com",
+    messagingSenderId: "507403774910",
+    appId: "1:507403774910:web:5e5ba3bf9b714a7bbde7ef"
+};
+
+export default firebaseConfig;
