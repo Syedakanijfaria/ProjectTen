328c142fcb11e6ab30806311755367be0a3be1e9
SKFaria <fariafaria737@gmail.com>
2021-10-20 11:12:12 +0600

    cry

--- src/Components/Package/Package.js
+++ src/Components/Package/Package.js
@@ -3,8 +3,41 @@ import './Package.css';
 
 const Package = () => {
     return (
-        <div>
+        <div className="container">
+            <div>
+                <h5>BECOME A PART OF US</h5>
+                <h1>OUR MEMBERSHIPS</h1>
+            </div>
 
+            <div className="row row-cols-1 row-cols-md-3 g-4 mt-5">
+                <div className="col">
+                    <div className="card text-white bg-dark mb-3">
+                        <div className="card-header">Header</div>
+                        <div className="card-body">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card text-white bg-dark mb-3">
+                        <div className="card-header">Header</div>
+                        <div className="card-body">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card text-white bg-dark mb-3">
+                        <div className="card-header">Header</div>
+                        <div className="card-body">
+                            <h5 className="card-title">Dark card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
